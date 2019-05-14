1b1c87f9f7f0b80d5660fb3f8e7652fe444ba3b3
wenjen10 <wenjen10@yahoo.com>
2019-05-14 09:07:48 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string greeting = "Hello World";
+
+            Console.WriteLine(greeting);
+
+
+            int number = 1;
+
+            Console.WriteLine(number);
+        }
+    }
+}
